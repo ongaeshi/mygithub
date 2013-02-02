3d04ec750d74cbe13e762d7a46c5b258faf5ae28
ongaeshi <ongaeshi0621@gmail.com>
2013-02-02 16:21:52 +0900

    Delete magic string

--- lib/mygithub/github_accessor.rb
+++ lib/mygithub/github_accessor.rb
@@ -9,6 +9,7 @@ require 'github_api'
 
 module Mygithub
   class GithubAccessor
+    attr_reader :username
     attr_reader :token
     
     def initialize(username, token)
@@ -19,14 +20,14 @@ module Mygithub
       # @github = Github.new(:oauth_token => @token)
       @github = Github.new do |config|
         config.endpoint    = 'https://api.github.com/'
-        config.oauth_token = token                      # @todo 何故か'@token'にすると動かない
+        config.oauth_token = token                      # @memo 何故か'@token'にすると動かない
         config.adapter     = :net_http
         config.ssl         = {:verify => false}
       end      
     end
 
     def repo_names
-      @github.repos.list(:user => 'ongaeshi').map {|r| r.full_name}
+      @github.repos.list(:user => @username).map {|r| r.full_name}
     end
   end
 end
